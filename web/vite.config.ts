74147e49c3f2a44a30b7ed9e69a3113c4cce157b
gatcl <clwucong@163.com>
2023-06-05 09:03:53 +0800

    引入新包，新配置

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src') // 路径别名
-    },
-    extensions: ['.js', '.json', '.ts'] // 使用路径别名时要省略的后缀名
-  }
+        plugins: [
+                vue(),
+                AutoImport({
+                        resolvers: [ElementPlusResolver()],
+                }),
+                Components({
+                        resolvers: [ElementPlusResolver()],
+                }),
+        ],
+        resolve: {
+                alias: {
+                        "@": resolve(__dirname, "src"), // 路径别名
+                },
+                extensions: [".js", ".json", ".ts"], // 使用路径别名时要省略的后缀名
+        },
 })
